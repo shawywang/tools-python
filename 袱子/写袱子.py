1766ac544a73a7670218e026c473d0359f6ae3e3
“wangxiao” <shaw.y.wang@foxmail.com>
2026-05-19 13:22:47 +0800

    例行24

--- 袱子/写袱子.py
+++ 袱子/写袱子.py
@@ -17,34 +17,39 @@ from PIL import Image, ImageDraw, ImageFont
 
 ps = platform.system().lower()
 
-wangxiao_wangyunhui: List[List[str]] = [
+rank_blank: List[int] = [86, 86, 86, 86]  # 列间隔
+
+wangyunhui: List[List[str]] = [
     ["孙(男/女)王(骁/云慧)", "祖父王公(讳)汝上老大人一位收用"],
     ["曾孙(男/女)王(骁/云慧)", "曾祖(父/母)王公(讳)(守玺/怀美)老(大/孺)人二位收用"],
     ["玄孙(男/女)王(骁/云慧)", "高祖(父/母)王公(讳)(尚安/陈君)老(大/孺)人二位收用"],
 ]
+wangyunhui_size: List[int] = [24, 43, 43, 43, 43]  # 每列文字大小，[0]为最右列
 
 wangxiong: List[List[str]] = [
     ["百期化钱 孝男王永", "雄/开/跃", "孝媳", "张秀华/刘方胜/刘翠英", "孝孙", "男/女", "王定海/毛勇/刘位/王雪莲/王雪萍", "孝孙", "媳/婿", "陈晓丽/侯富馨", "等具"],
-    ["谨具冥财 七封奉/上"],
+    ["  谨具冥财 七封奉/上"],
     ["故慈母王", "宅/", "朱君寄春老", "孺/", "人一位收用"],
-    ["坟前化纳"],
+    ["  坟前化纳"],
     ["天运甲辰年二月十六日敬献"],
 ]
+wangxiong_size: List[int] = [24, 43, 43, 43, 43]  # 每列文字大小，[0]为最右列
+
 wangxiao: List[List[str]] = [
     ["酬恩了愿化钱 下民姓氏王永骁"],
-    ["  情因本年中闰六月十八日请愿祈求二房东徐龙武退还/  车位办理费四百八十元特虔诚告曰愿躬赴九华山朝地藏/  并化宝帛敬奉土地言毕即应果蒙神佑诚心酬谢/正钱十五封俯伏上奉"],
+    ["  情因本年中闰六月十八日请愿祈求二房东徐龙武退还/  车位办理费四百八十元特虔诚告曰愿躬赴九华山朝地藏/  并化宝帛敬奉土地言毕即应果蒙神佑诚心酬谢/  正钱十五封俯伏上奉"],
     ["九华山幽冥教主地藏菩萨摩诃萨莲座下鉴纳/本境土地福德正神祠前收用"],
-    ["保佑清太/吉祥如意", "路旁化帛"],
+    ["  ", "保佑清太/吉祥如意", "路旁化帛"],
     ["天运乙巳年闰六月廿九日了愿是实"],
 ]
+wangxiao_size: List[int] = [40, 26, 36, 40, 40]  # 每列文字大小，[0]为最右列
 
 
 class Handle:
-    def __init__(self, content: List[List[str]]):
+    def __init__(self, content: List[List[str]], size: List[int]):
         self.content = content
-        self.img_path = "/Users/wangxiao/Downloads/1.webp"
-        self.font_size = 26
-        self.font_size_central = 35
+        self.font_size = size
+        self.img_path = "/Users/wangxiao/Downloads/1.jpeg"
         self.font_path = "/Users/wangxiao/Library/Fonts/KaiTi-YiMa.ttf"
         self.font_path2 = "/Users/wangxiao/Library/Fonts/康熙字典体.otf"
         self.width = 700
@@ -52,8 +57,7 @@ class Handle:
         self.blank = 10  # 四边空白像素
         self.horizon_count: List[int] = []  # 每列的并排数
         self.vert_count: List[int] = []  # 每列的长度
-        self.line_index_x: List[int] = [0, 0, 0, 0, 0]  # 每一列绘制起点x
-        self.line_index_y: List[int] = [0, 0, 0, 0, 0]  # 每一列绘制起点y
+        self.line_index_x: List[int] = []  # 每一列绘制起点x
 
     def horizontal_count(self):  # 水平方向
         for i, l in enumerate(self.content):
@@ -79,20 +83,28 @@ class Handle:
             self.vert_count.append(char_count)
 
     def gen_font_size(self):
-        vertical_font_size_max = (self.height - self.blank * 2) // max(self.vert_count)
-        horizontal_font_size_max = (self.width - self.blank * 2) // sum(self.horizon_count) - 1
-        self.font_size = min(vertical_font_size_max, horizontal_font_size_max)
-        self.font_size_central = (self.height - self.blank * 2) // self.vert_count[2]  # 中央列，强制顶格起头、上下顶满
+        vertical_min = (self.height - self.blank * 2) // max(self.vert_count)
+        vertical_max = (self.height - self.blank * 2) // min(self.vert_count)
+        horizontal_max = (self.width - self.blank * 2) // sum(self.horizon_count)
+        print(f"\n建议字体最大值（某列）：{max(vertical_max, vertical_min, horizontal_max)}")
+        print(f"建议字体最小值（某列）：{min(vertical_max, vertical_min, horizontal_max)}")
+        print(f"当前字体设置：{self.font_size}")
+        char_width: int = 0  # 文字一共所占宽度
+        for i, s in enumerate(self.font_size):
+            char_width += self.horizon_count[i] * s
+        blank = (self.width - self.blank * 2 - char_width) // (len(self.content) - 1)
+        print(f"\n建议平均列间隔：{blank}")
+        for i in range(len(rank_blank) - 1):
+            rank_blank[i] = blank
+        print(f"当前列间隔设置：{rank_blank}")
 
     def gen_index(self):
-        half = self.horizon_count[2] // 2 * self.font_size_central
-
-        self.line_index_x[0] = 0.5 * (self.width - self.blank - self.horizon_count[0] * self.font_size)
-        self.line_index_x[1] = 0.5 * (self.width - self.blank - (self.horizon_count[0] + self.horizon_count[1]) * self.font_size)
-        self.line_index_x[2] = self.width + half
-        self.line_index_x[3] = 0.5 * (self.blank - half - self.font_size + self.horizon_count[3] * self.font_size)
-        self.line_index_x[4] = 0.5 * (self.blank - half - self.font_size + (self.horizon_count[3] + self.horizon_count[4]) * self.font_size)
-        print()
+        right: int = self.width - self.blank - self.font_size[0]
+        for i, f in enumerate(self.font_size):
+            if i > 0:
+                right = right - (self.horizon_count[i - 1] - 1) * self.font_size[i - 1] - rank_blank[i - 1] - self.font_size[i]
+            left = right - (self.horizon_count[i] - 1) * self.font_size[i]
+            self.line_index_x.append(left + (right - left) // 2)
 
     def draw(self):
         image = Image.new("RGB", (self.width, self.height), "white")
@@ -105,44 +117,32 @@ class Handle:
             width=3,  # 边框宽度
             radius=0,  # 圆角半径
         )
-        palette_img = Image.new("P", (1, 1))  # 调色板
-        palette_img.putpalette(
-            [
-                0, 0, 0,  # 黑色
-                255, 0, 0,  # 红色
-                26, 170, 47,  # 绿色
-                255, 255, 255,  # 白色
-                255, 204, 153,  # 浅橙色
-                95, 95, 95,  # 灰色
-                221, 221, 221,  # 浅灰色
-            ]
-        )
-        fixed_image = image.quantize(palette=palette_img, dither=Image.Dither.NONE)
-        fixed_image.save(
+
+        image.save(
             fp=self.img_path,
             optimize=True,
             compress_level=5,
             dpi=(72, 72),
-            format="WEBP",
+            format="JPEG",
             quality=80,
         )
         for i, l in enumerate(self.content):
-            self.draw_line(l, self.line_index_x[i], 0)
+            self.draw_line(i, l)
 
-    def draw_line(self, line: List[str], x: int, y=0):
-        cur_y = y
+    def draw_line(self, i: int, line: List[str]):
+        cur_y = 0
         for sep in line:
-            cur_x = x
+            cur_x = self.line_index_x[i]
             line_num = sep.count("/") + 1  # 列数
             if line_num > 1:
                 line_half = line_num // 2
                 if line_num % 2 == 0:  # 偶数列
-                    cur_x += self.font_size // 2 + (line_half - 1) * self.font_size
+                    cur_x += self.font_size[i] // 2 + (line_half - 1) * self.font_size[i]
                 elif line_num % 2 == 1:
-                    cur_x += line_half * self.font_size
-            cur_y = self.sep(color="black", text=sep, x=cur_x, y=cur_y)
+                    cur_x += line_half * self.font_size[i]
+            cur_y = self.sep(i=i, color="black", text=sep, x=cur_x, y=cur_y)
 
-    def sep(self, text: str, color: str, x: int, y: int):
+    def sep(self, i: int, text: str, color: str, x: int, y: int):
         max_y = y
         img = Image.open(self.img_path).convert('RGB')
         draw = ImageDraw.Draw(img)
@@ -151,10 +151,10 @@ class Handle:
         for c in text:
             if c == "/":
                 now_y = y
-                now_x -= self.font_size
+                now_x -= self.font_size[i]
                 continue
-            draw.text((now_x, now_y), c, font=ImageFont.truetype(self.font_path, self.font_size), fill=color)
-            now_y += self.font_size  # 字的高度
+            draw.text((now_x, now_y), c, font=ImageFont.truetype(self.font_path, self.font_size[i]), fill=color)
+            now_y += self.font_size[i]  # 字的高度
             if now_y > max_y:
                 max_y = now_y
         img.save(self.img_path)
@@ -163,7 +163,7 @@ class Handle:
 
 def main():
     # print(f"CairoSVG版本：{cairosvg.__version__}")
-    h = Handle(wangxiao)
+    h = Handle(wangxiong, wangxiong_size)
     h.horizontal_count()
     h.vertical_count()
 
